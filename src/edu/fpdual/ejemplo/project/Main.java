679f58301f5c3d851f12ab2c4a891995c9b57a54
natalia.castillo.munoz <mnacamu@gmail.com>
2022-04-07 10:00:59 +0200

    añado src

---
+++ src/edu/fpdual/ejemplo/project/Main.java
@@ -0,0 +1,11 @@
+package edu.fpdual.ejemplo.project;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String saludo = "Hola";
+    int numero;
+    int p, h, j;
+    System.out.println(saludo);
+  }
+}
